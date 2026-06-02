00cbc0b6fc10f8a03329e9c7a22000be41d694dc
Gil Santos <gilsantos.engineer@gmail.com>
2026-06-02 16:25:06 +0200

    IDE Setup and Project cleanup

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,6 +4,9 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+// 1. Importa os pacotes do Prettier
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -18,5 +21,15 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
     },
+    // 2. Adiciona o plugin do Prettier
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    // 3. Ativa a regra para apontar erros de formatação
+    rules: {
+      'prettier/prettier': 'error',
+    },
   },
+  // 4. Aplica a configuração que desativa os conflitos no fim do array
+  eslintConfigPrettier,
 ])
