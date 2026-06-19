43c83e62158746564a77307f0b0093da129ad2c0
Gil Santos <gilsantos.engineer@gmail.com>
2026-06-19 16:17:39 +0200

    "adding a banner to userProfile"

--- frontend/src/layouts/Profile/UserProfileLayout.tsx
+++ frontend/src/layouts/Profile/UserProfileLayout.tsx
@@ -3,9 +3,9 @@ import Header from "../../components/Header";
 
 const UserProfileLayout = () => {
   return (
-    <div className="flex flex-col min-h-dvh min-w-dvw bg-stone-50 overflow-x-hidden">
+    <div className="flex flex-col min-h-dvh min-w-dvw bg-stone-50">
       <Header />
-      <div className="flex flex-col items-center w-full pb-20 pt-8 px-4">
+      <div className="flex flex-col items-center w-full pb-20">
         <Outlet />
       </div>
     </div>
